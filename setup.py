ad5f1364ce44e93ffc088d60818776cb3fd3bfd8
Mäel Le Garrec <mael.le.garrec@cern.ch>
2019-10-11 17:39:22 +0200

    Bump version: 1.0.4 → 1.0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="generic-parser",
-    version="1.0.3",
+    version="1.0.5",
     description="A parser for arguments and config-files that also allows direct python input.",
     long_description=README,
     long_description_content_type="text/markdown",
