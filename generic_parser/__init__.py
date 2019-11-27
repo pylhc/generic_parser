b1b291383b35c058b666d6ea4551e5e950bc8a95
Mäel Le Garrec <mael.le.garrec@cern.ch>
2019-11-27 15:05:49 +0100

    Bump version: 1.0.5 → 1.0.6

--- generic_parser/__init__.py
+++ generic_parser/__init__.py
@@ -1,4 +1,4 @@
 # Expose the main functionalities
 from generic_parser.entrypoint_parser import EntryPoint, EntryPointParameters, entrypoint
 from generic_parser.tools import DotDict
-__version__ = "1.0.5"
+__version__ = "1.0.6"
