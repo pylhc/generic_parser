eb28f3f0fc5cfb6ab1295c02f48838fd928e403b
JoschD <26184899+JoschD@users.noreply.github.com>
2021-11-16 17:19:06 +0000

    zenodo file and version (#52)

--- generic_parser/__init__.py
+++ generic_parser/__init__.py
@@ -5,7 +5,7 @@ from generic_parser.tools import DotDict
 __title__ = "generic_parser"
 __description__ = "A parser for arguments and config-files that also allows direct python input."
 __url__ = "https://github.com/pylhc/generic_parser"
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 __author__ = "pylhc"
 __author_email__ = "pylhc@github.com"
 __license__ = "MIT"
