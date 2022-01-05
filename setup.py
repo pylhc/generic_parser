7c823ca55f7a5f69224514a63ef5ba2b1cdb52cb
Felix Soubelet <felix.soubelet@protonmail.com>
2022-01-05 11:51:48 +0100

    add 3.10 to CI, remove EOL 3.6, update readme and classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pathlib
 from setuptools import setup, find_packages
 
 # Name of the module
-MODULE_NAME = 'generic_parser'
+MODULE_NAME = "generic_parser"
 
 
 # The directory containing this file
@@ -32,40 +32,36 @@ DEPENDENCIES = []
 
 # Extra dependencies for tools
 EXTRA_DEPENDENCIES = {
-                      'test': ['pytest>=5.2',
-                               'pytest-cov>=2.6',
-                               'hypothesis>=4.36.2',
-                               'attrs>=19.2.0'],
-                      'doc': ['sphinx',
-                              'travis-sphinx',
-                              'sphinx_rtd_theme']
-                      }
+    "test": ["pytest>=5.2", "pytest-cov>=2.6", "hypothesis>=4.36.2", "attrs>=19.2.0"],
+    "doc": ["sphinx", "travis-sphinx", "sphinx_rtd_theme"],
+}
 
 # The text of the README file
 with README.open("r") as docs:
     long_description = docs.read()
 
-# This call to setup() does all the work
 setup(
-    name=ABOUT_GENERIC_PARSER['__title__'],
-    version=ABOUT_GENERIC_PARSER['__version__'],
-    description=ABOUT_GENERIC_PARSER['__description__'],
+    name=ABOUT_GENERIC_PARSER["__title__"],
+    version=ABOUT_GENERIC_PARSER["__version__"],
+    description=ABOUT_GENERIC_PARSER["__description__"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=ABOUT_GENERIC_PARSER['__url__'],
-    author=ABOUT_GENERIC_PARSER['__author__'],
-    author_email=ABOUT_GENERIC_PARSER['__author_email__'],
-    license=ABOUT_GENERIC_PARSER['__license__'],
+    url=ABOUT_GENERIC_PARSER["__url__"],
+    author=ABOUT_GENERIC_PARSER["__author__"],
+    author_email=ABOUT_GENERIC_PARSER["__author_email__"],
+    license=ABOUT_GENERIC_PARSER["__license__"],
     python_requires=">=3.6",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    packages=find_packages(include=('generic_parser',)),
+    packages=find_packages(include=("generic_parser",)),
     install_requires=DEPENDENCIES,
-    tests_require=EXTRA_DEPENDENCIES['test'],
-    extras_require=EXTRA_DEPENDENCIES
+    tests_require=EXTRA_DEPENDENCIES["test"],
+    extras_require=EXTRA_DEPENDENCIES,
 )
